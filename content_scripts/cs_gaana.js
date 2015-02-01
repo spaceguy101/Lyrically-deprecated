785c6c35742efd369bcfe92de62a59eee12244d5
singham007 <shreyasponkshe12345@gmail.com>
2015-02-01 21:11:47 +0530

    Organised

--- content_scripts/cs_gaana.js
+++ content_scripts/cs_gaana.js
@@ -11,12 +11,18 @@ function fetchTrackInfo(){
       return;
   }
   
+   songs = div_trackInfo.getElementsByClassName("songName");
+  if (songs.length > 0){
+      Name = songs[0].innerText;
+      albumElement = div_trackInfo.getElementsByClassName("albumNamePl");
+      album = (albumElement.length > 0)?albumElement[0].innerText:"";
+  }else{
       tx=$("#tx");
       if(tx.length === 0) return;
       Name = tx.get(0).firstChild.nodeValue.trim();
       album = tx.find("span").eq(0).find("a").text().trim();
       Artist1 = tx.find("span").eq(1).find("a").eq(0).text().trim();
-  
+  }
 
   if (Name === "Gaana Promotional"){Name = prevName;}
   }
--- mywindow.js
+++ mywindow.js
@@ -144,14 +144,14 @@ function getLyricsFromLyricWikiURL(songURL) {
 				success : function(songData, songStatus) {
 					
 					lyrics = getLyricsFromRawHtml_wikia(songData);
-					$('.scrollbar').perfectScrollbar('update');
+					
 					if (lyrics.length === 0) {
 						throw ('No lyrics found');
 					} else {
 						document.getElementById('main').innerHTML = lyrics + '</p> Source <a href="'
 								+ songURL + '" target="_blank">LyricWiki.  </a>'
 								+ 'by-<a href="https://plus.google.com/+ShreyasPonkshe1/" target="_blank">Shreyas Ponkshe :)</a>.';
-						
+						$('.scrollbar').perfectScrollbar('update');
 					}
 					
 				}
@@ -183,28 +183,7 @@ function getLyricsFromLyricMintURL(songURL) {
 */ 
 
 
-function getLyricsFromLyricMastiURL(songURL) {
-	$
-			.ajax({
-				url : songURL,
-				type : 'GET',
-				success : function(songData, songStatus) {
-					
-					lyrics = getLyricsFromRawHtml_masti(songData);
-					$('.scrollbar').perfectScrollbar('update');
-					if (lyrics.length === 0) {
-						throw ('No lyrics found on mint');
-					} else {
-						document.getElementById('main').innerHTML = lyrics + '</p> Source <a href="'
-								+ songURL + '" target="_blank">LyricMint.com  </a>'
-								+ 'by-<a href="https://plus.google.com/+ShreyasPonkshe1/" target="_blank">Shreyas Ponkshe :)</a>.';
-						
-					}
-					
-					
-				}
-			});
-}
+
 
 
 
@@ -227,16 +206,6 @@ function getLyricsFromRawHtml_mint(data)
 			.html();
 }
 */
-
-
-function getLyricsFromRawHtml_masti(data) 
-{
-	var filter = function() 
-	{return this.nodeType === Node.TEXT_NODE|| $(this).is(' br, i, b, strong, em');};
-	return $('<div>').append(
-			$(data).find('#lcontent1').contents().filter(filter)).remove()
-			.html();
-}
 
 
 function getArtistFromMusicBrainz(title, album) {
@@ -302,31 +271,3 @@ $.ajax({
 }
 */ 
 
-//For Lyrics masti.com
-function getLyricsFromLyricsMasti(title,artist) {
-
-$.ajax({
-			url: 'https://ajax.googleapis.com/ajax/services/search/web',
-			data: {v:'1.0',q: 'site:www.lyricsmasti.com -"Page Ranking Information"' + title },
-			dataType: 'jsonp',
-			type: 'GET',
-			error: function(){},
-			success: function(googledata){
-				if(googledata.responseData.results[0].unescapedUrl){
-				url_lyricsMasti = googledata.responseData.results[0].unescapedUrl ;
-				
-				document.getElementById('main').innerHTML = '</p> Try URL <a href="'
-								+ url_lyricsMasti + '" target="_blank">'
-									+ title
-									+ '" by "'
-									+ artist +'</a>';
-									
-				getLyricsFromLyricMastiURL(url_lyricsMasti)	;	
-				}
-				else{
-					document.getElementById('main').innerHTML='Cant find on mint';
-				}
-							
-				
-			}});
-}
